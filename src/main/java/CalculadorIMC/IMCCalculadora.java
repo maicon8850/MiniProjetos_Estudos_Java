90b54ec9848682cccde07091aaf2361384265cfd
maicon-douglas <maicon.douglas3527@gmail.com>
2024-10-13 09:40:27 -0300

    feat: adicionar classe IMCCalculadora com método de cálculo do IMC

--- src/main/java/CalculadorIMC/IMCCalculadora.java
+++ src/main/java/CalculadorIMC/IMCCalculadora.java
@@ -1,4 +1,9 @@
 package CalculadorIMC;
-
+// Esta classe é responsável por calcular o IMC com base no peso e na altura.
 public class IMCCalculadora {
+
+    //metodo para calcular o IMC
+    public double calcularIMC (double peso, double altura){
+        return peso / (altura * altura);
+    }
 }
