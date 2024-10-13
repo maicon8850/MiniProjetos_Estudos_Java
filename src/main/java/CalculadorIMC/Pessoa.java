8eecff2dcfc552bba0429a8b9b4d0af53a6d2cb5
maicon-douglas <maicon.douglas3527@gmail.com>
2024-10-13 09:32:04 -0300

    Implementa classe Pessoa com nome, peso, altura, construtor e getters

--- src/main/java/CalculadorIMC/Pessoa.java
+++ src/main/java/CalculadorIMC/Pessoa.java
@@ -1,5 +1,25 @@
 package CalculadorIMC;
 
 public class Pessoa {
+    private String nome;
+    private double peso;
+    private double altura;
+
+    //construtor
+    public Pessoa ( String nome, double peso, double altura) {
+        this.nome = nome;
+        this.peso = peso;
+        this.altura = altura;
+    }
+    // get para retorna valor
+    public String getNome() {
+        return nome;
+    }
+    public double getPeso() {
+        return peso;
+    }
+    public double getAltura() {
+        return altura;
+    }
 
 }
