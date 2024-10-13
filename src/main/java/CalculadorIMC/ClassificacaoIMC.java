1ec9f8fbc8191688a8b02597bd539398ee3563d9
maicon-douglas <maicon.douglas3527@gmail.com>
2024-10-13 10:46:19 -0300

    feat: adicionar classe ClassificaçãoIMC com lógica de classificação baseada no valor do IMC

--- src/main/java/CalculadorIMC/ClassificacaoIMC.java
+++ src/main/java/CalculadorIMC/ClassificacaoIMC.java
@@ -1,4 +1,19 @@
 package CalculadorIMC;
 
+//Esta classe avalia o valor do IMC e retorna a faixa correspondente (Abaixo do peso, Normal, Sobrepeso, ou Obesidade).
+
 public class ClassificacaoIMC {
+
+    // Método para obter a classificação com base no IMC
+    public String obterClassificacao(double imc) {
+        if (imc < 18.5) {
+            return "Abaixo do peso";
+        } else if (imc >= 18.5 && imc < 24.9) {
+            return "Peso normal";
+        } else if (imc >= 25.0 && imc < 29.9) {
+            return "Sobrepeso";
+        } else {
+            return "Obesidade";
+        }
+    }
 }
